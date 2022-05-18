45e6de95489721b8e492059c732c4f61898a1150
riyanks <94890854+riyanks@users.noreply.github.com>
2022-05-18 13:45:37 +0700

    update

--- menu.py
+++ menu.py
@@ -1,6 +1,23 @@
 
-###################################### Syafira ############################################3
-def display_Menu(self, screen):
+import pygame , os
+from pygame import mixer
+
+#class untuk menampilkan menu
+class Menu():
+    def __init__ (self, gambar, x, y, W, H, animasi = 10):
+        self.width_M , self.height_M = W , H
+        self.image_original = pygame.transform.scale(gambar, (self.width_M, self.height_M))
+        self.image_elevation = pygame.transform.scale(gambar, (120, 120))
+        self.image = pygame.transform.scale(gambar, (self.width_M, self.height_M))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+        self.dinamic_elevation = animasi
+        self.original_y = y
+        self.butten_sound = mixer.Sound('Assets\sound_button.wav')
+        self.curs_sound = mixer.Sound('Assets\sound_curs.wav')
+        self.check = True
+    def display_Menu(self, screen):
         play = False
 
         self.rect.y = self.original_y
